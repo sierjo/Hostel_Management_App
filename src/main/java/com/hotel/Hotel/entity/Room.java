975010529658fc35279cda65f8e24029c6b9a251
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-04-26 20:06:44 +0300

    Create Room and User Entities
    Add initial Entities: Room, User
    
    Create Entities/Models

---
+++ src/main/java/com/hotel/Hotel/entity/Room.java
@@ -0,0 +1,36 @@
+package com.hotel.Hotel.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
+
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+    private String roomType;
+    private String roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice='" + roomPrice + '\'' +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
+}
