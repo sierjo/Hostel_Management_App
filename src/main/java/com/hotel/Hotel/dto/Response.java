e49496a2cc46b5e94217dc01a161f35fa0583b23
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-04-29 20:35:45 +0300

    This class used for sending back status, messages, tokens, 	and optionally user, room, booking data or lists.

---
+++ src/main/java/com/hotel/Hotel/dto/Response.java
@@ -0,0 +1,26 @@
+package com.hotel.Hotel.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+
+    private int statusCOde;
+    private String message;
+    private String token;
+    private String role;
+    private String bookingConfirmationCode;
+    private String expirationTime;
+
+    private UserDTO user;
+    private RoomDTO room;
+    private BookingDTO booking;
+
+    private List<UserDTO> userList;
+    private List<RoomDTO> roomList;
+    private List<BookingDTO> bookingList;
+}
