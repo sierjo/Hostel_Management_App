205f89c6837864a4b23decd7879d1c0d3e843c6f
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-04-29 20:18:43 +0300

    Contains check-in/out dates, guest count, and related user and room information.

---
+++ src/main/java/com/hotel/Hotel/dto/BookingDTO.java
@@ -0,0 +1,32 @@
+package com.hotel.Hotel.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hotel.Hotel.entity.Room;
+import com.hotel.Hotel.entity.User;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+
+public class BookingDTO {
+
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+
+    private UserDTO user;
+    private RoomDTO room;
+}
