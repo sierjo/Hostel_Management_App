24b44f789d3cbadfba79378e6c5e59bc165d08cb
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-04-29 20:33:35 +0300

    Contains email and password fields with basic validation

---
+++ src/main/java/com/hotel/Hotel/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.hotel.Hotel.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Data
+
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private  String passsword;
+}
