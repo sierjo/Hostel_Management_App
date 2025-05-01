4ba467e17504f08f6cb33514145ab3e16a491575
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-05-01 16:47:06 +0300

    Search for a user in the database by email.
    If the user is not found, an OurException is thrown with the message:
    "USerName/Email not Found".
    
    If found - the User object is returned and used for further authentication.

---
+++ src/main/java/com/hotel/Hotel/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.hotel.Hotel.service;
+
+import com.hotel.Hotel.exception.OurException;
+import com.hotel.Hotel.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(()->new OurException("USerName/Email not Found"));
+    }
+}
