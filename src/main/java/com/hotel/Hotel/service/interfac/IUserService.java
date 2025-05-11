cf0a817ea3ec389d04393283c65202981c3cf05d
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-05-11 21:10:09 +0300

    feat: add IUserService interface for user-related operations

---
+++ src/main/java/com/hotel/Hotel/service/interfac/IUserService.java
@@ -0,0 +1,22 @@
+package com.hotel.Hotel.service.interfac;
+
+import com.hotel.Hotel.dto.LoginRequest;
+
+import com.hotel.Hotel.dto.Response;
+import com.hotel.Hotel.entity.User;
+
+public interface IUserService {
+    Response register(User user);
+
+    Response login(LoginRequest loginRequest);
+
+    Response getAllUser();
+
+    Response getUserBookingHistory(String userId);
+
+    Response deleteUser(String userId);
+
+    Response getUserById(String userId);
+
+    Response getMyInfo(String userId);
+}
