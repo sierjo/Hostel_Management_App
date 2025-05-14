a6f14f89861dba7ae3523744c6ae6bcf12fd8b6e
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-05-14 20:30:13 +0300

    refactor(booking): change return type of 	findByBookingConfirmationCode to Optional
    - Updated method to reflect unique confirmation code constraint
    
    - Improves semantic correctness and
    simplifies null-checking logic in service layer

--- src/main/java/com/hotel/Hotel/repo/BookingRepository.java
+++ src/main/java/com/hotel/Hotel/repo/BookingRepository.java
@@ -5,9 +5,10 @@ import com.hotel.Hotel.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByRoomId(Long roomId);
-    List<Booking> findByBookingConfirmationCode(String confirmationCOde);
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCOde);
     List<Booking> findByUserId(Long userId);
 }
