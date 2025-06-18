709d512c6d7d48e6793bb50b128ff318b503dbef
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-06-18 16:28:03 +0300

    feat: - Created a REST controller 'AuthController' - Mapped the controller to the base path '/auth'.
    Added two POST endpoints:
    
    1. POST /auth/register:
       - Accepts a 'User' object from the request body.
       - Delegates the user registration logic to the 'IUserService.register()' method.
       - Returns a 'Response' wrapped in a 'ResponseEntity', using the status from
       'response.getStatusCOde()'.
    
    2. POST /auth/login:
       - Accepts a 'LoginRequest' object containing login credentials.
       - Delegates authentication to 'IUserService.login()'.
       - Returns a 'Response' wrapped in a 'ResponseEntity', with the corresponding HTTP status.

---
+++ src/main/java/com/hotel/Hotel/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.hotel.Hotel.controller;
+
+import com.hotel.Hotel.dto.LoginRequest;
+import com.hotel.Hotel.dto.Response;
+import com.hotel.Hotel.entity.User;
+import com.hotel.Hotel.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+}
