d0e669d28da0aae23c04d09ac935aca5b0055488
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-06-20 17:21:02 +0300

    feat: RoomController class is added but it will be refactor!!

--- src/main/java/com/hotel/Hotel/controller/RoomController.java
+++ src/main/java/com/hotel/Hotel/controller/RoomController.java
@@ -1,4 +1,81 @@
 package com.hotel.Hotel.controller;
 
+import com.hotel.Hotel.dto.Response;
+import com.hotel.Hotel.service.interfac.IBookingService;
+import com.hotel.Hotel.service.interfac.IRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+    @Autowired
+    private IRoomService roomService;
+    @Autowired
+    private IBookingService iBookingService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> addNewRoom(
+            @RequestParam(value = "photo", required = false) MultipartFile photo,
+            @RequestParam(value = "roomType", required = false) String roomType,
+            @RequestParam(value = "roomPrice", required = false) BigDecimal roomPrice,
+            @RequestParam(value = "roomDescription", required = false) String roomDescription
+    ) {
+        if (photo == null || photo.isEmpty() || roomType == null || roomType.isBlank() || roomPrice == null || roomType.isBlank()) {
+            Response response = new Response();
+            response.setStatusCOde(400);
+            response.setMessage("Please provide values for all fields(photo, roomType, roomPrice)");
+            return ResponseEntity.status(response.getStatusCOde()).body(response);
+        }
+        Response response = roomService.addNewRoom(photo, roomType, roomPrice, roomDescription);
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllRooms() {
+        Response response = roomService.getAllRooms();
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+
+    @GetMapping("/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomType();
+    }
+
+    @GetMapping("/room-by-id/{roomId}")
+    public ResponseEntity<Response> getRoomById(@PathVariable long roomId) {
+        Response response = roomService.getRoomById(roomId);
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+
+    @GetMapping("/all-available-rooms")
+    public ResponseEntity<Response> getAvailableRooms() {
+        Response response = roomService.getAllAvailableRooms();
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
+
+    @GetMapping("/available-rooms-by-date-and-type")
+    public ResponseEntity<Response> getAvailableRoomsByDateAndType(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkInDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOutDate,
+            @RequestParam(required = false) String roomType
+    ) {
+        if (checkInDate == null || roomType == null || roomType.isBlank() || checkOutDate == null) {
+            Response response = new Response();
+            response.setStatusCOde(400);
+            response.setMessage("Please provide values for all fields(checkInDate, roomType, checkOutDate)");
+            return ResponseEntity.status(response.getStatusCOde()).body(response);
+        }
+        Response response = roomService.addNewRoom(photo, roomType, roomPrice, roomDescription);
+        return ResponseEntity.status(response.getStatusCOde()).body(response);
+    }
 }
