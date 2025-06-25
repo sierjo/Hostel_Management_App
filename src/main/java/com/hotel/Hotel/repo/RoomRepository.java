1528e95ea4663da09fb101b1917bc471dbf7b1c9
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-06-25 16:45:45 +0300

    feat: add Room–Booking relationship, fix RoomRepository query.
    - Added @OneToMany relationship from Room to Booking
    - - - - Fixed JPQL syntax in get All Available Rooms() query.

--- src/main/java/com/hotel/Hotel/repo/RoomRepository.java
+++ src/main/java/com/hotel/Hotel/repo/RoomRepository.java
@@ -15,7 +15,7 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
             "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
     List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 
-    @Query("SELECT r FROM r WHERE r.id NOT IN (SELECT  b.room.id FROM Booking b)")
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
     List<Room> getAllAvailableRooms();
 }
 
